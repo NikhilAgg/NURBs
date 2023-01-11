89dfe2fcde754eebea8c843609a2703f67325d68
NikhilAgg <nikhilaggarwal64@gmail.com>
2023-01-11 14:52:07 +0000

    Added Taylor Tests for control_point_deriv

---
+++ Gmsh/deriv_tests/2D/single/control_point_taylor_test.py
@@ -0,0 +1,88 @@
+from bspline.bspline_curve import BSplineCurve, BSpline2DGeometry
+from bspline.gmsh_utils import create_bspline_curve_mesh_file
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def calc(epsilon, ind_der):
+    points = [
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0],
+        [-1, 1, 0],
+        [-1, 0, 0],
+        [-1, -1, 0],
+        [0, -1, 0],
+        [1, -1, 0],
+        [1, 0, 0]
+    ]
+
+    weights = [1, 1/2**0.5, 1, 1/2**0.5, 1, 1/2**0.5, 1, 1/2**0.5, 1]
+    knots = [0, 1/4, 1/2, 3/4, 1]
+    multiplicities = [3, 2, 2, 2, 3]
+
+    circle = BSplineCurve(
+        points,
+        weights,
+        knots,
+        multiplicities,
+        2
+    )
+    circle.set_uniform_lc(1e-2)
+
+    t = np.linspace(0, 1, 100)
+    y = np.array([circle.calculate_point(u) for u in t])
+    dy = np.array([[circle.derivative_wrt_ctrl_point(ind_der, u)] for u in t])
+    der = y + dy * epsilon
+
+    points = np.array([
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0],
+        [-1, 1, 0],
+        [-1, 0, 0],
+        [-1, -1, 0],
+        [0, -1, 0],
+        [1, -1, 0],
+        [1, 0, 0]
+    ], dtype=np.float32)
+
+    for i in range(3):
+        points[ind_der, i] += epsilon[0][i]
+
+    circle = BSplineCurve(
+        points,
+        weights,
+        knots,
+        multiplicities,
+        2
+    )
+    circle.set_uniform_lc(1e-2)
+
+    t = np.linspace(0, 1, 100)
+    y = np.array([circle.calculate_point(u) for u in t])
+
+    return y, der
+
+
+y_tay = []
+x_tay = []
+
+ep_step = 0.01
+ind_der = 1
+coord = np.array([[0, 1, 0]])
+
+for k in range(4):
+    epsilon = ep_step * k
+    epsilon_coord = coord * epsilon
+    y, der = calc(epsilon_coord, ind_der)
+    error = 0
+    for i in range(len(y)):
+        error += np.linalg.norm(y[i] - der[i])**2
+
+    y_tay.append(error)
+    x_tay.append(epsilon**2)
+
+
+plt.plot(x_tay, y_tay)
+plt.show()
---
+++ Gmsh/deriv_tests/3D/single/control_point_taylor.py
@@ -0,0 +1,119 @@
+from bspline.bspline_curve import BSplineSurface
+from bspline.gmsh_utils import create_bspline_volume_mesh_file
+from pathlib import Path
+import numpy as np
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+import os
+
+def calc(epsilon, i_der, j_der):
+    points = [
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0],
+        [-1, 1, 0],
+        [-1, 0, 0],
+        [-1, -1, 0],
+        [0, -1, 0],
+        [1, -1, 0],
+        [1, 0, 0],
+        [1, 0, 1],
+        [1, 1, 1],
+        [0, 1, 1],
+        [-1, 1, 1],
+        [-1, 0, 1],
+        [-1, -1, 1],
+        [0, -1, 1],
+        [1, -1, 1],
+        [1, 0, 1]
+    ]
+
+    weights = [1, 1/2**0.5, 1, 1/2**0.5, 1, 1/2**0.5, 1, 1/2**0.5, 1, 1, 1/2**0.5, 1, 1/2**0.5, 1, 1/2**0.5, 1, 1/2**0.5, 1]
+    knotsU = [0, 1/4, 1/2, 3/4, 1]
+    knotsV = [0, 1]
+    multiplicitiesU = [3, 2, 2, 2, 3]
+    multiplicitiesV = [2, 2]
+
+    cylinder = BSplineSurface(
+        points,
+        weights,
+        knotsU,
+        knotsV,
+        multiplicitiesU,
+        multiplicitiesV,
+        degreeU = 2,
+        degreeV = 1
+    )
+    cylinder.set_uniform_lc(1e-2)
+
+    t = np.linspace(0, 1, 100)
+    s = np.linspace(0, 1, 100)
+    y = np.array([cylinder.calculate_point(u, v) for u, v in np.array(np.meshgrid(t, s)).T.reshape(-1,2)])
+    dy = np.array([[cylinder.derivative_wrt_ctrl_point(i_der, j_der, u, v)] for u, v in np.array(np.meshgrid(t, s)).T.reshape(-1,2)])
+    der = y + dy * epsilon
+
+    points = np.array([
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0],
+        [-1, 1, 0],
+        [-1, 0, 0],
+        [-1, -1, 0],
+        [0, -1, 0],
+        [1, -1, 0],
+        [1, 0, 0],
+        [1, 0, 1],
+        [1, 1, 1],
+        [0, 1, 1],
+        [-1, 1, 1],
+        [-1, 0, 1],
+        [-1, -1, 1],
+        [0, -1, 1],
+        [1, -1, 1],
+        [1, 0, 1]
+    ], dtype=np.float32)
+
+    for i in range(3):
+        points[j_der * cylinder.nU + i_der, i] += epsilon[0][i]
+
+    cylinder = BSplineSurface(
+        points,
+        weights,
+        knotsU,
+        knotsV,
+        multiplicitiesU,
+        multiplicitiesV,
+        degreeU = 2,
+        degreeV = 1
+    )
+    cylinder.set_uniform_lc(1e-2)
+
+    t = np.linspace(0, 1, 100)
+    s = np.linspace(0, 1, 100)
+    y = np.array([cylinder.calculate_point(u, v) for u, v in np.array(np.meshgrid(t, s)).T.reshape(-1,2)])
+
+    return y, der
+
+
+y_tay = []
+x_tay = []
+
+ep_step = 0.01
+i_der = 3
+j_der = 1
+coord = np.array([[0, 1, 0]])
+
+for k in range(4):
+    epsilon = ep_step * k
+    epsilon_coord = coord * epsilon
+    y, der = calc(epsilon_coord, i_der, j_der)
+    error = 0
+    for i in range(len(y)):
+        error += np.linalg.norm(y[i] - der[i])**2
+
+    y_tay.append(error)
+    x_tay.append(epsilon**2)
+
+
+plt.plot(x_tay, y_tay)
+plt.show()
