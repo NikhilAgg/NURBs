Fixed taylor test for u derivatives

Gmsh/deriv_tests/2D/single/u/taylor.py
@@ -2,7 +2,7 @@ from bspline.bspline_curve import BSplineCurve
 import numpy as np
 import matplotlib.pyplot as plt
 
-def calc(epsilon, u, order):
+def calc(u, order):
     points = [
         [1, 0, 0],
         [1, 1, 0],
@@ -31,25 +31,30 @@ def calc(epsilon, u, order):
     y = circle.calculate_point(u)
     dy = circle.derivative_wrt_u(u, order)
 
+    return y, dy
+
+def get_der(y, dy, epsilon):
     der = y.copy()
     for i in range(1, order+1):
-        temp = dy[i]*epsilon**i
+        temp = dy[i]*(epsilon**i) / np.math.factorial(i)
         der += temp
 
-    return y, der
-
+    return der
 
 y_tay = []
 x_tay = []
 
 ep_step = 0.01
 u = 0.5
-order = 1
+order = 2
 coord = np.array([[0, 1, 0]])
+
+y_orig, dy = calc(u, order=2)
 
 for k in range(7):
     epsilon = ep_step * k
-    y, der = calc(epsilon, u, order)
+    y = calc(u+epsilon, order)[0]
+    der = get_der(y_orig, dy, epsilon)
     error = 0
     for i in range(len(y)):
         error += np.linalg.norm(y[i] - der[i])**2